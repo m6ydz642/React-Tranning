8fd9e45a157c507ac6908385de7fb2b491823ea7
m6ydz642 <m6ydz642@gmail.com>
2020-11-02 03:43:03 +0900

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+ render() {
+  return (
+    <div className="App">
+      클래스 방식으로 변경
+      깃 테스트
+    </div>
+  );
+}
+}
+
+export default App;
